55228abb94000b4998115ba63f1b1ace57863e4f
Matthias Luppi <53262016+mluppi@users.noreply.github.com>
2021-08-04 18:52:49 +0200

    Add support to specify an alternate input directory
    This feature is used for multilingual coaches where language-specific
    assets/files can be placed in the main input directory and the original
    coach data resides in the alternate input directory.

--- flatifier/src/test/java/eu/smesec/platform/flatifier/LauncherTest.java
+++ flatifier/src/test/java/eu/smesec/platform/flatifier/LauncherTest.java
@@ -1,12 +1,15 @@
 package eu.smesec.platform.flatifier;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Locale;
 
 import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test for {@link Launcher}.
@@ -15,9 +18,14 @@ import static org.junit.Assert.assertThrows;
  */
 public class LauncherTest {
 
-    public static final String ARG_DIR_VALID = Paths.get("src", "test", "resources", "fhnw").toString();
+    public static final String ARG_DIR_VALID = Paths.get("src", "test", "resources", "fhnw-full").toString();
     public static final String ARG_OUT_VALID = Paths.get("target", "test-output", "launcher-test-fhnw-flat.xml").toString();
     public static final String ARG_OUT_NOEXISTS = Paths.get("target", "test-output", "noexists", "launcher-test-fhnw-flat.xml").toString();
+
+    @Before
+    public void setUp() throws IOException {
+        Files.deleteIfExists(Paths.get(ARG_OUT_VALID));
+    }
 
     @Test
     public void testNoArgs() {
@@ -34,7 +42,7 @@ public class LauncherTest {
         assertThrows(IllegalArgumentException.class, () -> Launcher.launch(new String[]{ARG_DIR_VALID, ARG_OUT_NOEXISTS}));
     }
 
-    @Test(expected = Test.None.class)
+    @Test
     public void testFlatification() {
         try {
             Files.createDirectories(Paths.get(ARG_OUT_VALID).getParent());
@@ -42,6 +50,8 @@ public class LauncherTest {
             throw new IllegalStateException("Could not prepare test", e);
         }
         Launcher.launch(new String[]{ARG_DIR_VALID, ARG_OUT_VALID});
+
+        assertTrue(Files.exists(Paths.get(ARG_OUT_VALID)));
     }
 
 }
